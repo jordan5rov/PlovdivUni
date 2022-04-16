dc9fc02777f244482ef863033520cd62f50f169f
jordan5rov <jordan5rov@gmail.com>
2022-04-17 00:07:20 +0300

    Solved Task 1

--- OOP2/Task1/Program.cs
+++ OOP2/Task1/Program.cs
@@ -1,3 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
+/*
+ * Write a program that reads two arrays from the console and compares them element by element and by their lenght.
+ */
 
-Console.WriteLine("Hello, World!");
+int[] ReadArray(int n)
+{
+    int[] arr = new int[n];
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return arr;
+}
+
+Console.Write("Enter the lenght of the arrays: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Enter the numbers for the first array");
+int[] nums1 = ReadArray(n);
+Console.Write("Enter the numbers for the second array");
+int[] nums2 = ReadArray(n);
+
+for (int i = 0; i < n; i++)
+{
+    if (nums1[i] == nums2[i] && nums1.Length == nums2.Length)
+    {
+        Console.WriteLine("The elements are equal");
+    }
+    else
+    {
+        Console.WriteLine("The elements are not equal");
+    }
+}
