776cd9e483f1d988dd4632d668639fdf4abf7994
jordan5rov <jordan5rov@gmail.com>
2022-06-23 01:09:49 +0300

    Added exam prep for Algorithms and Data Structures

---
+++ PreExam-AlgorithmsAndDataStructures/PreExam-AlgorithmsAndDataStructures/Algorithms.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace PreExam_AlgorithmsAndDataStructures
+{
+    public class Algorithms
+    {
+        public static string BinarySearch(int[] arr, int number)
+        {
+            var startIdx = 0;
+            var endIdx = arr.Length - 1;
+            while (startIdx <= endIdx)
+            {
+                var midIdx = (startIdx + endIdx) / 2;
+                if (arr[midIdx] == number)
+                {
+                    return $"The number {number} has been found at index {midIdx}";
+                }
+
+                if (number > arr[midIdx])
+                {
+                    startIdx = midIdx + 1;
+                }
+                else
+                {
+                    endIdx = midIdx - 1;
+                }
+            }
+
+            return $"The number {number} has not been found in the array";
+        }
+
+        public static void SelectionSort(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                var minIdx = i;
+                var minNum = arr[i];
+                
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[j] < minNum)
+                    {
+                        minNum = arr[j];
+                        minIdx = j;
+                    }
+                }
+                
+                Swap(arr, i, minIdx);
+            }
+        }
+
+        public static void Swap(int[] arr, int first, int second)
+        {
+            var temp = arr[first];
+            arr[first] = arr[second];
+            arr[second] = temp;
+            
+        }
+    }
+}
---
+++ PreExam-AlgorithmsAndDataStructures/PreExam-AlgorithmsAndDataStructures/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace PreExam_AlgorithmsAndDataStructures
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            var sortedNumbers = new[] {0, 1, 21, 33, 45, 45, 61, 71, 72, 73};
+            Console.WriteLine(Algorithms.BinarySearch(sortedNumbers, 5));
+
+            /*
+             Read array in single line split by space
+             Example: 1 5 6 7 8
+            */
+            
+            var numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Algorithms.SelectionSort(numbers);
+            Console.WriteLine(string.Join(" ", numbers) );
+        }
+    }
+}
