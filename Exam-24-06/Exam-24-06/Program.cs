4dd1f30062271a2ed00a183a1a78bec6fc111d78
jordan5rov <jordan5rov@gmail.com>
2022-06-30 02:16:37 +0300

    Added 1 more exercise

--- Exam-24-06/Exam-24-06/Program.cs
+++ Exam-24-06/Exam-24-06/Program.cs
@@ -167,7 +167,76 @@ class Program
             }
 
             return 2 * g(n - 1) * g(n) + g(n + 1);
+            
+        }
+        //####################################################################################
+        // Умножение на дълги цели числа, представени с масив от цифрите им в десетична бройна система.
 
+        // Multiplies str1 and str2, and prints result.
+        static int[] Multiply(int[] num1, int[] num2)
+        {
+            int len1 = num1.Length;
+            int len2 = num2.Length;
+
+            if (len1 == 0 || len2 == 0)
+            {
+                return new int[0];
+            }
+
+            // will keep the result number in vector
+            // in reverse order
+            int[] result = new int[len1 + len2];
+
+            // Below two indexes are used to
+            // find positions in result.
+            int i_n1 = 0;
+            int i_n2 = 0;
+            int i;
+
+            // Go from right to left in num1
+            for (i = len1 - 1; i >= 0; i--)
+            {
+                int carry = 0;
+                int n1 = num1[i];
+
+                // To shift position to left after every
+                // multipliccharAtion of a digit in num2
+                i_n2 = 0;
+
+                // Go from right to left in num2            
+                for (int j = len2 - 1; j >= 0; j--)
+                {
+                    // Take current digit of second number
+                    int n2 = num2[j];
+
+                    // Multiply with current digit of first number
+                    // and add result to previously stored result
+                    // charAt current position.
+                    int sum = n1 * n2 + result[i_n1 + i_n2] + carry;
+
+                    // Carry for next itercharAtion
+                    carry = sum / 10;
+
+                    // Store result
+                    result[i_n1 + i_n2] = sum % 10;
+
+                    i_n2++;
+                }
+
+                // store carry in next cell
+                if (carry > 0)
+                {
+                    result[i_n1 + i_n2] += carry;
+                }
+
+                // To shift position to left after every
+                // multipliccharAtion of a digit in num1.
+                i_n1++;
+            }
+
+            Array.Reverse(result);
+
+            return result;
         }
     }
 }
