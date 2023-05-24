3d7eb36bcab52caf26a4eb45719f30844f9a9761
jordan5rov <jordan5rov@gmail.com>
2023-05-24 15:47:12 +0300

    Added project that works with Nasa and weather apis

---
+++ APIConnect/app.py
@@ -0,0 +1,43 @@
+import requests
+
+# API keys (if required)
+WEATHER_API_KEY = '44f27f4754b929a585de742d9e6b365a'
+NASA_API_KEY = 'V5k2i3RvCupnFRbwkhrdQGt3YMfCKJW40U2caCgZ'
+
+
+# Weather API integration (OpenWeather)
+def get_weather(city):
+    url = f'http://api.openweathermap.org/data/2.5/weather?q={city}&appid={WEATHER_API_KEY}'
+    response = requests.get(url)
+    data = response.json()
+
+    # Extract relevant information from the response
+    temperature = data['main']['temp']
+    weather = data['weather'][0]['description']
+    return temperature, weather
+
+
+# NASA API integration
+def get_apod():
+    url = f'https://api.nasa.gov/planetary/apod?api_key={NASA_API_KEY}'
+    response = requests.get(url)
+    data = response.json()
+
+    # Extract relevant information from the response
+    title = data['title']
+    explanation = data['explanation']
+    return title, explanation
+
+
+# Earth Status App
+def earth_status_app(city):
+    temperature, weather = get_weather(city)
+    print(f"Current weather in {city}: {temperature} K, {weather}")
+
+    title, explanation = get_apod()
+    print(f"Astronomy Picture of the Day: {title}")
+    print(explanation)
+
+
+# Example usage
+earth_status_app('New York')
